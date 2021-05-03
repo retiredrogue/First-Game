8f508a032c9656480e1c0c8dc7290c87510dabe4
retiredrogue <retiredroguegaming@gmail.com>
2021-05-03 18:54:14 -0400

    inventory prefab

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Inventory : MonoBehaviour {
+	public UIItemSlot[] slots;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	private void Start() {
+	}
+
+	// Update is called once per frame
+	private void Update() {
+	}
 }
