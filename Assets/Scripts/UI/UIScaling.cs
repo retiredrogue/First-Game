6a0cf700a9d28942f6b4669d541b45cee4f4064a
retiredrogue <retiredroguegaming@gmail.com>
2021-05-24 12:12:26 -0400

    ui scaling

---
+++ Assets/Scripts/UI/UIScaling.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScaling : MonoBehaviour {
+	private void Start() {
+
+
+		Debug.Log(Camera.main.pixelRect);
+} }
